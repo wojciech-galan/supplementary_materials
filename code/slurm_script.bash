2b7472b3f0cd3902939e5311550238e4e3be925c
wojciech-galan <wojciech.galan@uj.edu.pl>
2017-10-31 12:46:10 +0100

    mut_probas

--- code/slurm_script.bash
+++ code/slurm_script.bash
@@ -3,6 +3,12 @@
 #SBATCH -p plgrid-short
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=24
+
+mut_probas = 0.05 0.075 0.1 0.125 0.15 0.175 0.2
+
 cd wirusy/supplementary_materials/code/
 module load plgrid/tools/python/2.7.13
-python knn_ga_feature_selection.py 10 5 cxUniform 0.1 2 1 3 20 2 ../ga/ --cpus 24
+for mut_proba in $mut_probas
+do
+    python knn_ga_feature_selection.py 10 5 cxUniform $mut_proba 2 1 3 100 200 ../ga/ --cpus 24
+done
