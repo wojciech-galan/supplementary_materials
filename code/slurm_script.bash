4f8f09108eb6a180280730555c38cabf33cce256
wojciech-galan <wojciech.galan@uj.edu.pl>
2017-10-31 14:56:08 +0100

    m

--- code/slurm_script.bash
+++ code/slurm_script.bash
@@ -1,14 +1,29 @@
 #!/usr/bin/bash
 #SBATCH -A hostphylumpred
-#SBATCH -p plgrid-short
+#SBATCH -p plgrid
+#SBATCH --time=3-00:00:00
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem=12GB
 
-mut_probas = 0.05 0.075 0.1 0.125 0.15 0.175 0.2
+mut_probas='0.05 0.075 0.1 0.125 0.15 0.175 0.2'
+crossovers='cxOnePoint cxTwoPoint cxUniform'
 
 cd wirusy/supplementary_materials/code/
 module load plgrid/tools/python/2.7.13
-for mut_proba in $mut_probas
+for mean in {10..90..10}
 do
-    python knn_ga_feature_selection.py 10 5 cxUniform $mut_proba 2 1 3 100 200 ../ga/ --cpus 24
+    for crossover in $crossovers
+    do
+        for mut_proba in $mut_probas
+        do
+            for tournsize in {2..8..2}
+            do
+                for k in {1..7..2}
+                do
+                    python knn_ga_feature_selection.py $mean 5 $crossover $mut_proba $tournsize 1 $k 100 200 ../ga/ --cpus 12 >> repeat1
+                done
+            done
+        done
+    done
 done
