a0e4d0cb2dfd2ffdb52675c75115f57b4e3775b2
wojciech-galan <wojciech.galan@uj.edu.pl>
2017-12-01 14:08:54 +0100

    qda

--- code/ml_stuff.py
+++ code/ml_stuff.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn import neighbors
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import roc_auc_score
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 
 def binary_classification_evaluation(classes_proper, classes_probas, ids, positive_num, class_order):
@@ -57,14 +58,22 @@ def knn(X_train, X_test, y_train, neighbours):
     clf.fit(X_train, y_train)
     return clf.predict_proba(X_test), clf.classes_
 
+
+def qda(X_train, X_test, y_train):
+    clf = QuadraticDiscriminantAnalysis()
+    clf.fit(X_train, y_train)
+    return clf.predict_proba(X_test), clf.classes_
+
+
 def preprocess_results_for_given_splits_and_features(results):
     '''
     Scales MCC to range [0,1]
     :param results: numpy array [[mcc, auc], [mcc, auc],...]
     :return: scaled results
     '''
-    results[:,0] = (results[:,0] + 1) / 2
+    results[:, 0] = (results[:, 0] + 1) / 2
     return results
+
 
 def knn_for_given_splits_and_features(features_indexes, splits, positive_class, neighbours):
     results = []
@@ -73,6 +82,19 @@ def knn_for_given_splits_and_features(features_indexes, splits, positive_class, 
         l = l[:, features_indexes]
         t = t[:, features_indexes]
         res, class_order = knn(l, t, l_classes, neighbours)
+        evaluated = binary_classification_evaluation(t_classes, res, t_ids, positive_class, class_order)
+        results.append(evaluated)
+    results = preprocess_results_for_given_splits_and_features(np.array(results))
+    return np.mean(results[:, 0]), np.std(results[:, 0]), np.mean(results[:, 1]), np.std(results[:, 1])
+
+
+def qda_for_given_splits_and_features(features_indexes, splits, positive_class):
+    results = []
+    for split in splits:
+        l, t, l_classes, t_classes, l_ids, t_ids = split
+        l = l[:, features_indexes]
+        t = t[:, features_indexes]
+        res, class_order = qda(l, t, l_classes)
         evaluated = binary_classification_evaluation(t_classes, res, t_ids, positive_class, class_order)
         results.append(evaluated)
     results = preprocess_results_for_given_splits_and_features(np.array(results))
