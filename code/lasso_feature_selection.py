43f37d41543e49f1bb746df405f8bfdc071120f8
wojciech-galan <wojciech.galan@uj.edu.pl>
2017-12-21 13:11:50 +0100

    auc

--- code/lasso_feature_selection.py
+++ code/lasso_feature_selection.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import cPickle as pickle
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import roc_auc_score
 
 
 attributes_learn = pickle.load(open(os.path.join('..', 'datasets', 'attributes_learn.dump')))
@@ -16,7 +17,11 @@ features = pickle.load(open(os.path.join('..', 'datasets', 'features.dump')))
 
 model = LogisticRegressionCV(Cs=100, cv=indices, solver='liblinear', penalty='l1')
 model.fit(attributes_learn, classes_learn)
-print model.score(attributes_test, classes_test)
+print model.get_params()
+pos_probas = model.predict_proba(attributes_test)[:, 1]
+auc = roc_auc_score(classes_test, pos_probas, None)
+print "Acc =", model.score(attributes_test, classes_test)
+print 'AUC =', auc
 print model.coef_
 print model.intercept_
 print np.where(model.coef_)[1]
