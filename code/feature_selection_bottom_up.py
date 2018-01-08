438e649f20bcea72ba851b8fd46411639629466c
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-01-08 13:59:05 +0100

    some initial results

--- code/feature_selection_bottom_up.py
+++ code/feature_selection_bottom_up.py
@@ -14,15 +14,21 @@ from ml_stuff import knn_for_given_splits_and_features, qda_for_given_splits_and
 
 
 def check_initial_combinations(classifier_for_given_splits_and_features, all_feature_indices, splits,
-                               current_results={}):
+                               processed_combinations, pool_of_workers):
+    results = {}
     for i in range(1, 4):
+        keys_to_be_processed = []
         for indices_combination in itertools.combinations(all_feature_indices, i):
-            key = frozenset(indices_combination)
-            if key in current_results:
-                continue
-            current_results[key] = classifier_for_given_splits_and_features(indices_combination, splits, 0)
-            # print current_results
-    return current_results
+            key = indices_to_number(set(indices_combination))
+            if key not in processed_combinations:
+                keys_to_be_processed.append(key)
+        print len(keys_to_be_processed), "keys will be processed"
+        processed_results = pool_of_workers(
+            delayed(classifier_for_given_splits_and_features)(number_to_indices(key), splits, 0) for key in
+            keys_to_be_processed)
+        results.update({keys_to_be_processed[i]: processed_results[i] for i in range(len(keys_to_be_processed))})
+        processed_combinations.update(keys_to_be_processed)
+    return results
 
 
 def append_feature(all_feature_indices, splits, positive_class, curr_feature_indices, curr_results,
@@ -33,21 +39,21 @@ def append_feature(all_feature_indices, splits, positive_class, curr_feature_ind
     while not_present_features_indices:
         feature_index = random.choice(not_present_features_indices)
         curr_feature_indices.append(feature_index)
-        key = frozenset(curr_feature_indices)
+        key = indices_to_number(set(curr_feature_indices))
         if not key in curr_results:
             curr_results[key] = classifier_for_given_splits_and_features(curr_feature_indices, splits, positive_class)
             remove_feature(curr_feature_indices, splits, positive_class, key, curr_results,
                            classifier_for_given_splits_and_features)
-            print key, curr_results[key][-4:]
+            print number_to_indices(key), curr_results[key][-4:]
         not_present_features_indices.remove(feature_index)
 
 
 def remove_feature(splits, positive_class, neighbours, curr_feature_indices_stable_copy,
                    curr_results, classifier_for_given_splits_and_features):
-    for index in curr_feature_indices_stable_copy:
+    for index in curr_feature_indices_stable_copy:  # todo ale to mają być słownik + set
         curr_feature_indices = curr_feature_indices_stable_copy[:]
         curr_feature_indices.remove(index)
-        key = frozenset(curr_feature_indices)
+        key = indices_to_number(set(curr_feature_indices))
         if key not in curr_results:
             curr_results[key] = classifier_for_given_splits_and_features(curr_feature_indices, splits, positive_class)
             print '(removing)', key, curr_results[key]
@@ -57,9 +63,11 @@ def sort_results_according_to_values(results, function_to_process_values):
     return sorted(results.iteritems(), key=lambda x: function_to_process_values(x[1]), reverse=True)
 
 
-def climb_up(classifier_for_given_splits_and_features, all_feature_indices, splits, current_results):
+def climb_up(classifier_for_given_splits_and_features, all_feature_indices, splits, procesed_keys, current_results,
+             max_best_results):
     while True:
         sorted_results = sort_results_according_to_values(current_results, individual_fitness)
+        raise Exception('ae to ma być dict')
         ch = random.choice([0, 1, 2])
         if ch == 0:
             i = 0
@@ -84,6 +92,7 @@ def number_to_indices(num):
         num /= 2
     return [i for i, x in enumerate(l) if x]
 
+
 def determine_last_result_num(directory):
     res_list = []
     for f in os.listdir(directory):
@@ -94,54 +103,56 @@ def determine_last_result_num(directory):
     except ValueError:  # res_list is empty
         return None
 
-def save_results(results, directory, pool_of_workers, keys=None):
-    if keys == None:
-        keys = results.keys()
-    numbers_from_corresponding_keys = pool_of_workers(delayed(indices_to_number)(key) for key in keys)
-    with open(os.path.join(directory, 'keys.dump'), 'w') as f:
-        pickle.dump(numbers_from_corresponding_keys, f)
-    with open(os.path.join(directory, 'keys_copy.dump'), 'w') as f:
-        pickle.dump(numbers_from_corresponding_keys, f)
+
+def save_results(results, directory, keys=None):
     # pattern for results filename:results.dump_%d, results_copy.dump_%d
     last_result_num = determine_last_result_num(directory)
     if last_result_num is None:
         result_num = 0
     else:
         result_num = last_result_num + 1
-    czy najpierw zapisywać klucze, czy wyniki?
+    with open(os.path.join(directory, 'results.dump_%d' % result_num), 'w') as f:
+        pickle.dump(results, f)
+    with open(os.path.join(directory, 'results_copy.dump_%d' % result_num), 'w') as f:
+        pickle.dump(results, f)
+    if keys == None:
+        keys = results.keys()
+    with open(os.path.join(directory, 'keys.dump'), 'w') as f:
+        pickle.dump(keys, f)
+    with open(os.path.join(directory, 'keys_copy.dump'), 'w') as f:
+        pickle.dump(keys, f)
 
 
-def load_results(directory, pool_of_workers):
-
+def load_results(directory):
     print "Loading results from disk"
     last_results_num = determine_last_result_num(directory)
     if last_results_num is None:
         print "no results"
-        return [], []
+        return {}, set()
     try:
-        with open(os.path.join(directory, "results.dump_%d"%last_results_num)) as f:
+        with open(os.path.join(directory, "results.dump_%d" % last_results_num)) as f:
             results = pickle.load(f)
     except:
         print "cannot load original result file, loading copy"
-        os.remove(os.path.join(directory, "results.dump_%d"%last_results_num))
+        os.remove(os.path.join(directory, "results.dump_%d" % last_results_num))
         try:
-            with open(os.path.join(directory, "results_copy.dump_%d"%last_results_num)) as f:
+            with open(os.path.join(directory, "results_copy.dump_%d" % last_results_num)) as f:
                 results = pickle.load(f)
         except:
             print "cannot load copy, loading prev results"
-            with open(os.path.join(directory, "results.dump_%d" % last_results_num-1)) as f:
+            with open(os.path.join(directory, "results.dump_%d" % last_results_num - 1)) as f:
                 results = pickle.load(f)
     try:
-        dumped_numbers = pickle.load(open(os.path.join(directory, 'keys.dump')))
+        keys = pickle.load(open(os.path.join(directory, 'keys.dump')))
     except:
-        dumped_numbers = pickle.load(open(os.path.join(directory, 'keys_copy.dump')))
-    keys = pool_of_workers(delayed(number_to_indices)(number) for number in dumped_numbers)
+        keys = pickle.load(open(os.path.join(directory, 'keys_copy.dump')))
     return results, keys
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Genetic algorithms for feature selection")
     parser.add_argument('res_dir', action='store', type=str, help='directory containing results')
+    parser.add_argument('n_proc', action='store', type=int, help='number_of_additional_processes')
     parser.add_argument('--infile', action='store', type=str, default=os.path.join('..', 'datasets', 'splits.dump'),
                         help='input file containing cv splits')
     parser.add_argument('--classifier', action='store', type=str, default='qda', choices=['qda', 'knn'],
@@ -151,20 +162,29 @@ if __name__ == '__main__':
     args = parser.parse_args()
     splits = pickle.load(open(args.infile))
     num_of_possible_features = len(splits[0][0][0])
+    num_of_best_res_after_sort = 100
     if args.classifier == 'qda':
         classifier_func = qda_for_given_splits_and_features
     elif args.classifier == 'knn':
         def is_odd(number):
             return number % 2
 
+
         if args.neighbours < 1 or not is_odd(args.neighbours):
             raise Exception("Number of neighbours has to be an odd number >= 1")
         classifier_func = rpartial(knn_for_given_splits_and_features, args.neighbours)
 
-    num = indices_to_number([1, 2, 4, 5, 7, 10])
-    print num, number_to_indices(num)
-    raise
     num_of_features = splits[0][0].shape[1]
-    initial_results = check_initial_combinations(classifier_func, range(num_of_features), splits)
-    print len(initial_results)
-    print initial_results
+    results, keys = load_results(args.res_dir)
+    # print results, keys
+    print len(results), len(keys)
+    import time
+    t = time.time()
+    with Parallel(n_jobs=args.n_proc) as parallel:
+        initial_results = check_initial_combinations(classifier_func, range(num_of_features), splits, keys, parallel)
+        results.update(initial_results)
+        results = dict(sort_results_according_to_values(results, individual_fitness)[:num_of_best_res_after_sort])
+        save_results(results, args.res_dir, keys)
+        print len(results), len(keys)
+    print time.time() -t
+    # climb_up(classifier_func, range(num_of_features), splits, procesed_keys, current_results, num_of_best_res_after_sort)
