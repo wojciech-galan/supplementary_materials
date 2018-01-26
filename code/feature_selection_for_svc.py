868e95cace13b826ca0d00d5ef4e690ac78b412a
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-01-26 12:32:05 +0100

    RFECV with C grid

--- code/feature_selection_for_svc.py
+++ code/feature_selection_for_svc.py
@@ -6,24 +6,10 @@ import cPickle as pickle
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.feature_selection import *
-from sklearn.model_selection import ParameterGrid
 from sklearn.metrics import make_scorer
 from sklearn.metrics import roc_auc_score
 from ml_stuff import binary_classification_evaluation
 from ml_stuff import preprocess_results_for_given_splits_and_features
-from feature_selection_bottom_up import number_to_indices, indices_to_number
-
-
-# class Result(object):
-#     def __init__(self, C, res, feature_indices):
-#         super(Result, self).__init__()
-#         self.C = C
-#         self.res = res
-#         self.fitness = individual_fitness(res)
-#         self.features = indices_to_number(feature_indices)
-#
-#     def __repr__(self):
-#         return 'Result(%f, %s, %s)' % (self.C, str(self.res), str(number_to_indices(self.features)))
 
 
 def scorer_function(y_true, y_predicted):
@@ -48,10 +34,20 @@ attributes_test = pickle.load(open(os.path.join('..', 'datasets', 'attributes_te
 classes_test = pickle.load(open(os.path.join('..', 'datasets', 'classes_test.dump')))
 indices = pickle.load(open(os.path.join('..', 'datasets', 'cv_indices.dump')))
 
+res_dir = os.path.join('..', 'svm_res')
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+
 scorer = make_scorer(scorer_function, needs_proba=True)
-estimator = SVC(kernel='linear', probability=True)
-selector = RFECV(estimator, cv=indices, scoring=scorer)
-selector.fit(attributes_learn, classes_learn)
-support  = selector.get_support()
-print cv_for_given_splits_and_features(selector.estimator, attributes_learn, classes_learn, indices, support) #featureindices potrzebne?
-print roc_auc_score(classes_test, selector.predict_proba(attributes_test)[:,1])
+results = {}
+for C in 2**np.linspace(-5, 15, 21):
+    estimator = SVC(C=C, kernel='linear', probability=True)
+    selector = RFECV(estimator, cv=indices, scoring=scorer)
+    selector.fit(attributes_learn, classes_learn)
+    support = selector.get_support(indices=True)
+    results[C] = (cv_for_given_splits_and_features(selector.estimator, attributes_learn, classes_learn, indices, support), selector)
+
+pickle.dump(results, open(os.path.join(results, 'RFE.dump')))
+best_result = max(results.items(), key=lambda item:item[1][0])
+best_selector = best_result[1][1]
+print roc_auc_score(classes_test, best_selector.predict_proba(attributes_test)[:,1])
