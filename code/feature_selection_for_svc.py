3b55a4a5027cad3798137b7428acf89a7931c539
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-01-26 15:27:51 +0100

    initial code for selectKbest

--- code/feature_selection_for_svc.py
+++ code/feature_selection_for_svc.py
@@ -6,8 +6,11 @@ import cPickle as pickle
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.feature_selection import RFECV
+from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.metrics import make_scorer
 from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from ml_stuff import binary_classification_evaluation
 from ml_stuff import preprocess_results_for_given_splits_and_features
 
@@ -20,8 +23,8 @@ def scorer_function(y_true, y_predicted):
 def cv_for_given_splits_and_features(estimator, X, y, split_indices, feature_indices):
     results = []
     for learn_indices, test_indices in split_indices:
-        l = X[learn_indices,:][:,feature_indices]
-        t = X[test_indices, :][:,feature_indices]
+        l = X[learn_indices, :][:, feature_indices]
+        t = X[test_indices, :][:, feature_indices]
         y_learn = y[learn_indices]
         y_test = y[test_indices]
         estimator.fit(l, y_learn)
@@ -40,17 +43,29 @@ res_dir = os.path.join('..', 'svm_res')
 if not os.path.exists(res_dir):
     os.makedirs(res_dir)
 
-scorer = make_scorer(scorer_function, needs_proba=True)
-results = {}
-for C in 2 ** np.linspace(-5, 15, 21):
-    estimator = SVC(C=C, kernel='linear', probability=True)
-    selector = RFECV(estimator, cv=indices, scoring=scorer)
-    selector.fit(attributes_learn, classes_learn)
-    support = selector.get_support(indices=True)
-    results[C] = (
-    cv_for_given_splits_and_features(selector.estimator, attributes_learn, classes_learn, indices, support), selector)
+# scorer = make_scorer(scorer_function, needs_proba=True)
+# results = {}
+# for C in 2 ** np.linspace(-5, 15, 21):
+#     estimator = SVC(C=C, kernel='linear', probability=True)
+#     selector = RFECV(estimator, cv=indices, scoring=scorer)
+#     selector.fit(attributes_learn, classes_learn)
+#     support = selector.get_support(indices=True)
+#     results[C] = (
+#     cv_for_given_splits_and_features(selector.estimator, attributes_learn, classes_learn, indices, support), selector)
+#
+# pickle.dump(results, open(os.path.join(results, 'RFE.dump')))
+# best_result = max(results.items(), key=lambda item: item[1][0])
+# best_selector = best_result[1][1]
+# print "RFE AUC:", roc_auc_score(classes_test, best_selector.predict_proba(attributes_test)[:, 1])
 
-pickle.dump(results, open(os.path.join(results, 'RFE.dump')))
-best_result = max(results.items(), key=lambda item: item[1][0])
-best_selector = best_result[1][1]
-print roc_auc_score(classes_test, best_selector.predict_proba(attributes_test)[:, 1])
+# TODo zmienić w publikacji na linear kernel
+
+# TODO zmienić w publikacji chi2 na f_classif
+
+kbest = SelectKBest(f_classif)  # TODO zmienić funkcję!
+pipeline = Pipeline([('kbest', kbest), ('svc', SVC(kernel='linear'))])  # TODO probas!
+grid_search = GridSearchCV(pipeline,
+                           {'kbest__k': range(attributes_learn.shape[1] - 1, 0, -1),
+                            'svc__C': 2 ** np.linspace(-5, 15, 21)},
+                           scoring=None, cv=indices, n_jobs=2)  # TODO zmienic scoring
+grid_search.fit(attributes_learn, classes_learn)
