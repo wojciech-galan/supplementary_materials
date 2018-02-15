05e726b09b7be4fd069b334027db3174ceae314e
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-02-15 12:26:04 +0100

    no message

--- code/feature_selection_for_svc_penalizedSVM.py
+++ code/feature_selection_for_svc_penalizedSVM.py
@@ -5,6 +5,7 @@ import os
 import warnings
 import cPickle as pickle
 import numpy as np
+import argparse
 from sklearn.svm import SVC
 from sklearn.metrics import roc_auc_score, matthews_corrcoef, accuracy_score
 from rpy2.robjects import numpy2ri
@@ -12,59 +13,68 @@ from rpy2.robjects import r
 from feature_selection_for_svc_biogram import svc_for_given_splits_and_features
 from ga_stuff import individual_fitness
 
-attributes_learn = pickle.load(open(os.path.join('..', 'datasets', 'attributes_learn.dump')))
-classes_learn = pickle.load(open(os.path.join('..', 'datasets', 'classes_learn.dump')))
-attributes_test = pickle.load(open(os.path.join('..', 'datasets', 'attributes_test.dump')))
-classes_test = pickle.load(open(os.path.join('..', 'datasets', 'classes_test.dump')))
-indices = pickle.load(open(os.path.join('..', 'datasets', 'cv_indices.dump')))
-splits = pickle.load(open(os.path.join('..', 'datasets', 'splits.dump')))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Genetic algorithms for feature selection")
+    parser.add_argument('--infile', action='store', type=str, default=os.path.join('..', 'datasets', 'splits.dump'),
+                        help='input file containing cv splits')
+    parser.add_argument('--outdir', action='store', type=str, default=os.path.join('..', 'svm_res'),
+                        help='directory for results')
+    parser.parse_args()
 
-lambdas = np.linspace(0.01, 0.05, 41)
+    # attributes_learn = pickle.load(open(os.path.join('..', 'datasets', 'attributes_learn.dump')))
+    # classes_learn = pickle.load(open(os.path.join('..', 'datasets', 'classes_learn.dump')))
+    # attributes_test = pickle.load(open(os.path.join('..', 'datasets', 'attributes_test.dump')))
+    # classes_test = pickle.load(open(os.path.join('..', 'datasets', 'classes_test.dump')))
+    # indices = pickle.load(open(os.path.join('..', 'datasets', 'cv_indices.dump')))
+    splits = pickle.load(open(parser.infile))
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    r.library('penalizedSVM')
+    lambdas = np.linspace(0.01, 0.05, 41)
 
-numpy2ri.activate()
-# in the first round of cross-validation 5 possible feature sets are selected
-feature_sets = []
-features_left = []
-for split in splits:
-    l_classes_negative = np.asarray([x or -1 for x in split[2]])
-    a_res = r['svm.fs'](split[0], l_classes_negative, fs_method='scad', lambda1_set=lambdas)
-    import pdb
-    pdb.set_trace()
-    model = a_res.rx2('model')
-    print model.rx('xind')[0]
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        r.library('penalizedSVM')
 
-    features_indices = [x - 1 for x in model.rx('xind')[0]]
-    feature_sets.append(np.array(features_indices))
-    # p_values = r.test_features(split[2], binarized_attributes_learn, quick=True)
-    # significant_features = [x for x in range(len(p_values)) if p_values[x] < 0.0001]
-    # feature_sets.append(np.array(significant_features))
-    # features_left.append(np.array(list(set(range(len(p_values))) - set(significant_features))))
+    numpy2ri.activate()
+    # in the first round of cross-validation 5 possible feature sets are selected
+    feature_sets = []
+    models=[]
+    for split in splits:
+        l_classes_negative = np.asarray([x or -1 for x in split[2]])
+        a_res = r['svm.fs'](split[0], l_classes_negative, fs_method='scad', lambda1_set=lambdas)
+        model = a_res.rx2('model')
+        print model.rx('xind')[0]
 
-# in the second round of cross-validation 5 possible feature sets are evaluated
-scores = []
-for features_indexes in feature_sets:
-    scores.append(individual_fitness(
-        svc_for_given_splits_and_features(features_indexes, splits, 0, kernel='linear', probability=True)))
-print scores  #
-max_score = max(scores)
-index = scores.index(max_score)
-max_features = feature_sets[index]
-res_dir = os.path.join('..', 'svm_res')
-if not os.path.exists(res_dir):
-    os.makedirs(res_dir)
+        features_indices = [x - 1 for x in model.rx('xind')[0]]
+        feature_sets.append(np.array(features_indices))
+        models.append(model)
+        pickle.dump(model, open(parser.outdir, 'w'))
+        raise
+        # p_values = r.test_features(split[2], binarized_attributes_learn, quick=True)
+        # significant_features = [x for x in range(len(p_values)) if p_values[x] < 0.0001]
+        # feature_sets.append(np.array(significant_features))
+        # features_left.append(np.array(list(set(range(len(p_values))) - set(significant_features))))
 
-pickle.dump(max_features, open(os.path.join(res_dir, 'best_features_penalizedSVM.dump'), 'w'))
-scores_for_left_features = []
+    # in the second round of cross-validation 5 possible feature sets are evaluated
+    scores = []
+    for features_indexes in feature_sets:
+        scores.append(individual_fitness(
+            svc_for_given_splits_and_features(features_indexes, splits, 0, kernel='linear', probability=True)))
+    print scores  #
+    max_score = max(scores)
+    index = scores.index(max_score)
+    max_features = feature_sets[index]
+    res_dir = os.path.join('..', 'svm_res')
+    if not os.path.exists(res_dir):
+        os.makedirs(res_dir)
 
-clf = SVC(kernel='linear', probability=True)
-clf.fit(attributes_learn, classes_learn)
-probas = clf.predict_proba(attributes_test)
-predicted = clf.predict(attributes_test)
+    pickle.dump(max_features, open(os.path.join(res_dir, 'best_features_penalizedSVM.dump'), 'w'))
+    scores_for_left_features = []
 
-print roc_auc_score(classes_test, probas[:,1])   # 0.982733136254
-print accuracy_score(classes_test, predicted)    # 0.944672131148
-print matthews_corrcoef(classes_test, predicted) # 0.889351731351
+    clf = SVC(kernel='linear', probability=True)
+    clf.fit(attributes_learn, classes_learn)
+    probas = clf.predict_proba(attributes_test)
+    predicted = clf.predict(attributes_test)
+
+    print roc_auc_score(classes_test, probas[:,1])   # 0.982733136254
+    print accuracy_score(classes_test, predicted)    # 0.944672131148
+    print matthews_corrcoef(classes_test, predicted) # 0.889351731351
---
+++ code/slurm_script_penalizedSVM.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+#SBATCH -A hostphylumpred
+#SBATCH -p plgrid
+#SBATCH --time=3-00:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=3GB
+module load plgrid/tools/python/2.7.13
+module load plgrid/apps/r/3.3.0
+
+export R_LIBS_USER="/net/people/plgalan/R/libs"
+
+infile="$SCRATCHDIR/datasets/splits.dump"
+outdir="$SCRATCHDIR/svm_res"
+
+subdir="svm_res"
+
+mkdir $SCRATCHDIR/datasets
+mkdir -p $SCRATCHDIR/$subdir
+mkdir -p $SLURM_SUBMIT_DIR/$subdir
+cp $SLURM_SUBMIT_DIR/datasets/splits.dump $SCRATCHDIR/datasets
+
+python code/feature_selection_for_svc_penalizedSVM.py --infile $infile --outdir
+#for mean in {10..90..10}
+#do
+#    for crossover in $crossovers
+#    do
+#        for mut_proba in $mut_probas
+#        do
+#            for tournsize in {2..8..2}
+#            do
+#                for k in {1..7..2}
+#                do
+#                    python code/knn_ga_feature_selection.py $mean 5 $crossover $mut_proba $tournsize 1 $k 100 200 $SCRATCHDIR/$subdir --infile $infile --cpus 6 --searchdir $SLURM_SUBMIT_DIR/$subdir
+#                done
+#                mv $SCRATCHDIR/$subdir/* $SLURM_SUBMIT_DIR/$subdir
+#            done
+#        done
+#    done
