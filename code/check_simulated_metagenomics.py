c08d25bc27816cdea74c64e18eb945521eff5675
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-09-07 15:31:04 +0200

    code moved to main

--- code/check_simulated_metagenomics.py
+++ code/check_simulated_metagenomics.py
@@ -113,47 +113,47 @@ def predict_based_on_fasta_file(fasta_seq, fasta_dir, gi_molecule, gi_host, i):
         result.append(p.communicate()[0].strip())
     return (result[0], result[1], result[2], result[3], gi_host[gi], gi)
 
+if __name__ == '__main__':
+    container_old = Container.fromFile(os.path.join('..', 'datasets', 'container_Fri_Oct__6_14:26:35_2017.dump'))
+    with_host_old = [v for v in container_old if v.host_lineage]
+    ids_old = [v.gi for v in with_host_old]
+    sys.modules['src'] = src
+    container_new = pickle.load(open(os.path.join('..', 'datasets', 'container_Mon_Aug_27_16:51:14_2018.dump')))
+    with_host_new = [v for v in container_new if v.host_lineage]
+    with_host_diff = [v for v in with_host_new if v.gi not in ids_old]
+    with_proper_host_ineage_diff = [v for v in with_host_diff if translate_host_to_infecting(v.host_lineage) in ('Eucaryota-infecting', 'phage')]
+    gi_host_map = {v.gi:translate_host_to_infecting(v.host_lineage) for v in with_proper_host_ineage_diff}
+    gi_molecule_map = {v.gi:v.molecule for v in with_proper_host_ineage_diff}
 
-container_old = Container.fromFile(os.path.join('..', 'datasets', 'container_Fri_Oct__6_14:26:35_2017.dump'))
-with_host_old = [v for v in container_old if v.host_lineage]
-ids_old = [v.gi for v in with_host_old]
-sys.modules['src'] = src
-container_new = pickle.load(open(os.path.join('..', 'datasets', 'container_Mon_Aug_27_16:51:14_2018.dump')))
-with_host_new = [v for v in container_new if v.host_lineage]
-with_host_diff = [v for v in with_host_new if v.gi not in ids_old]
-with_proper_host_ineage_diff = [v for v in with_host_diff if translate_host_to_infecting(v.host_lineage) in ('Eucaryota-infecting', 'phage')]
-gi_host_map = {v.gi:translate_host_to_infecting(v.host_lineage) for v in with_proper_host_ineage_diff}
-gi_molecule_map = {v.gi:v.molecule for v in with_proper_host_ineage_diff}
+    # assuming, that fasta sequences are stored in /tmp/seqs
+    old_dir = '/tmp/seqs'
+    new_dir = '/tmp/seqs2'
+    fasta_dir = '/tmp/fastas'
+    try:
+        os.makedirs(new_dir)
+    except OSError:
+        pass
+    try:
+        os.makedirs(fasta_dir)
+    except OSError:
+        pass
 
-# assuming, that fasta sequences are stored in /tmp/seqs
-old_dir = '/tmp/seqs'
-new_dir = '/tmp/seqs2'
-fasta_dir = '/tmp/fastas'
-try:
-    os.makedirs(new_dir)
-except OSError:
-    pass
-try:
-    os.makedirs(fasta_dir)
-except OSError:
-    pass
+    for gi in gi_host_map:
+        fasta = read_fasta_file(os.path.join(old_dir, gi))
+        write_fastas_to_a_file([fasta], os.path.join(new_dir, gi)) # change seq description to gi
+    subprocess.call('cat %s > /tmp/seq' % ' '.join([os.path.join(new_dir, gi) for gi in gi_host_map]), shell=True)
 
-for gi in gi_host_map:
-    fasta = read_fasta_file(os.path.join(old_dir, gi))
-    write_fastas_to_a_file([fasta], os.path.join(new_dir, gi)) # change seq description to gi
-subprocess.call('cat %s > /tmp/seq' % ' '.join([os.path.join(new_dir, gi) for gi in gi_host_map]), shell=True)
-
-evaluation_results = {}
-for length in (1000, ):
-    for error_rate in (0, ):
-        simulated_seq_path = 'blah_%f_%d.fastq'%(error_rate, length)
-        if not os.path.exists(simulated_seq_path):
-            cmd = 'wgsim /tmp/seq %s /dev/null -h -e %f -S 77 -d 0 -1 %d -N 100000'%(simulated_seq_path, error_rate, length)
-            print cmd
-            subprocess.call(cmd, shell=True)
-        fastqs = read_fastq_file(simulated_seq_path)
-        fastas = [fastq_to_fasta(fastq) for fastq in fastqs]
-        print "starting classifier evaluation"
-        results = Parallel(n_jobs=6)(delayed(predict_based_on_fasta_file)(fasta_seq=fasta, fasta_dir=fasta_dir, gi_molecule=gi_molecule_map, gi_host=gi_host_map, i=i) for i, fasta in enumerate(fastas))
-        evaluation_results[(length, error_rate)] = [Result(result[0], result[1], result[2], result[3], result[4], result[5]) for result in results]
-        pickle.dump(evaluation_results[(length, error_rate)], open(os.path.join('..', 'datasets', 'check_simmulated_metagenomics_results_%.1f_%d.dump'%(error_rate, length)), 'w'))
+    evaluation_results = {}
+    for length in (1000, ):
+        for error_rate in (0, ):
+            simulated_seq_path = 'blah_%f_%d.fastq'%(error_rate, length)
+            if not os.path.exists(simulated_seq_path):
+                cmd = 'wgsim /tmp/seq %s /dev/null -h -e %f -S 77 -d 0 -1 %d -N 100000'%(simulated_seq_path, error_rate, length)
+                print cmd
+                subprocess.call(cmd, shell=True)
+            fastqs = read_fastq_file(simulated_seq_path)
+            fastas = [fastq_to_fasta(fastq) for fastq in fastqs]
+            print "starting classifier evaluation"
+            results = Parallel(n_jobs=6)(delayed(predict_based_on_fasta_file)(fasta_seq=fasta, fasta_dir=fasta_dir, gi_molecule=gi_molecule_map, gi_host=gi_host_map, i=i) for i, fasta in enumerate(fastas))
+            evaluation_results[(length, error_rate)] = [Result(result[0], result[1], result[2], result[3], result[4], result[5]) for result in results]
+            pickle.dump(evaluation_results[(length, error_rate)], open(os.path.join('..', 'datasets', 'check_simmulated_metagenomics_results_%.1f_%d.dump'%(error_rate, length)), 'w'))
